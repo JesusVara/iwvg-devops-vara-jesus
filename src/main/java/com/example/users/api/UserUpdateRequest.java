095f5f2d3ff4b8187e2f911267858cd410477b38
Jesus Vara <eddywm@gmail.com>
2026-09-20 09:30:37 +0200

    Add endpoint: PUT /user/{id} first version without test

---
+++ src/main/java/com/example/users/api/UserUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.example.users.api;
+
+public record UserUpdateRequest(
+        String firstName,
+        String familyName,
+        String email,
+        String identity,
+        String address,
+        String city,
+        String province,
+        String postalCode
+) {
+}
